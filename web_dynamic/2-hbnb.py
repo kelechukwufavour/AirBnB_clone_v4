0a6a798132fcef32154fd4fa26611b6d3559d7b0
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2024-01-25 22:55:06 +0000

    ensuring it serves the same purpose as web dynamic

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -3,10 +3,11 @@ dropdown menus and rental listings"""
 from flask import Flask, render_template
 from models import storage
 import uuid
-app = Flask('web_dynamic')
+
+app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-
+@app.route('web_dynamic/2-hbnb.py')
 @app.route('/2-hbnb')
 def display_hbnb():
     """Generate page with popdown menu of states/cities"""
@@ -15,17 +16,15 @@ def display_hbnb():
     places = storage.all('Place')
     cache_id = uuid.uuid4()
     return render_template('2-hbnb.html',
-states=states,
-amenities=amenities,
-places=places,
-cache_id=cache_id)
-
+                           states=states,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cache_id)
 
 @app.teardown_appcontext
 def teardown_db(*args, **kwargs):
     """Close database or file storage"""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003)
